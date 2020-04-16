987e29e1c9e84bde5809ecf58c325a09756c7118
xtremerox <ramatheracer@gmail.com>
2020-04-16 12:44:42 +0530

    New update

---
+++ frontend/static/scriptsnon.js
@@ -0,0 +1,166 @@
+myusername = "";
+socket = "";
+temp_partner = "";
+var notif = new Audio('static/notif.mp3');
+$(document).ready(function () {
+    $tnc_block = $('#tncblock');
+    //$tnc_block.hide(); //debugging
+    $loading = $('#connecting');
+    $tnc_button = $('#tncbutton');
+    $tnc_accept = $('#tncaccept');
+    $chat_block = $('#chatblock');   
+    $username = $('#username'); 
+    $("#newcon").attr("disabled", true);
+    $tnc_button.on('click', function(){
+        if($tnc_accept.prop('checked')){
+            $tnc_block.slideUp("slow",function(){
+                $username.slideDown("slow",function(){
+                   // setTimeout(chaton, 3000);
+                });
+            });
+        }else{
+            $("#confwarn").html('<b style="color:red">Cant ignore this :P</b>');
+        }
+    });
+    $('#chatwindow').css('height', ($(window).height()/1.3));
+});
+
+
+function clearchatbox(){
+    $("#actualchat").html("");
+    $("#msg").val("");
+    $("#unamemsg").html("");
+}
+
+function chaton(){
+        clearchatbox();
+        $("#newcon").attr("disabled", true);
+        $.ajax({url: "/ajax/live", success: function(result){
+            $("#livecount").html(result);
+        }});
+        $username.slideUp("slow", function(){
+            socket=io("nojob.tibstack.com", {path : '/ajax/socket.io'});
+            // socket=io("localhost:3000", {path : '/socket.io'});
+            //socket=io("localhost:3000");
+        $loading.slideDown("slow",function(){
+            // add user to live and random roulette
+            //add user
+            socket.emit("init",{
+                username : myusername,
+                id : socket.id
+            });
+            socket.on("msg",function(message){
+                notif.play();
+                $("#actualchat").append($('<p class="stranger"></p>').text(message));
+                $('#actualchat').scrollTop($('#actualchat')[0].scrollHeight);
+            });
+            socket.on("partner",function(partner){
+                temp_partner = partner;
+                $loading.slideUp("slow", function(){
+                    $("#actualchat").append($('<p class="conmsg text-center"></p>').text("You are now talking to " + temp_partner.username));
+                    $chat_block.slideDown("slow");
+                });
+            });
+            socket.on("pat_disc",function(nothing){
+                $("#actualchat").append($('<p class="dismsg text-center"></p>').text(temp_partner.username + " has disconnected"));
+                $("#disconnect").attr("disabled", true);
+                $("#newcon").attr("disabled", false);
+                $("#msg").attr("disabled", true);
+                $("#send").attr("disabled", true);
+                temp_partner = "";
+            });
+        });
+    });
+}
+
+function clearchatbox(){
+    $("#actualchat").html("");
+    $("#msg").val("");
+    $("#unamemsg").html("");
+}
+
+function rechaton(){
+        clearchatbox();
+        $("#unamefield").attr("disabled", false);
+        $("#newcon").attr("disabled", true);
+        $.ajax({url: "/ajax/live", success: function(result){
+            $("#livecount").html(result);
+        }});
+        $chat_block.slideUp("slow", function(){
+        socket.connect();
+       // socket=io("localhost:3000", {path : '/socket.io'});
+        $loading.slideDown("slow",function(){
+            // add user to live and random roulette
+            //add user
+            socket.emit("init",{
+                username : myusername,
+                id : socket.id
+            });
+        });
+    });
+}
+
+$("#disconnect").click(function(){
+    $("#unamefield").attr("disabled", false);
+    $chat_block.slideUp("slow",function(){
+        socket.disconnect();
+        socket.off('pat_disc');
+        socket.off('msg');
+        socket.off('partner');
+        temp_partner="";
+        $username.slideDown("slow");
+        $("#newcon").attr("disabled", false);
+        clearchatbox();
+    });
+});
+
+$("#newcon").click(function(){
+    $("#unamefield").attr("disabled", false);
+    clearchatbox();
+    rechaton();
+    $("#disconnect").attr("disabled", false);
+    $("#msg").attr("disabled", false);
+    $("#send").attr("disabled", false);
+});
+
+$("#send").click(function(e){
+    e.preventDefault();
+    msg = {};
+    msg.sender = socket.id;
+    msg.username = myusername;
+    msg.to = temp_partner.id;
+    msg.message = $("#msg").val();
+    socket.emit('msg', msg);
+    $("#actualchat").append($('<p class="you"></p>').text($("#msg").val()));
+    $("#msg").val("");
+    $('#actualchat').scrollTop($('#actualchat')[0].scrollHeight);
+});
+
+
+//receiving
+//<p class="stranger"><b>Strange : </b>Strange Reply</p>
+
+$("#unbutton").click(function(){
+    myusername = $("#unamefield").val();
+    data = {}
+    data.myusername = myusername;
+    $.ajax({ 
+        url: '/ajax/adduser',
+        type: 'POST',
+		data: JSON.stringify(data),
+		contentType: 'application/json',
+        success: function(result)
+        {   if(result==='2'){
+                $("#unamemsg").html('<b style="color:red">Invalid Userame.</b>');
+            }
+            else if(result==='1'){
+                $("#unamemsg").html('<b style="color:green">All Set. Redirecting to chat ..</b>');
+                $("#unamefield").attr("disabled", true);
+                setTimeout(chaton, 2000);
+            }
+            else{
+                $("#unamemsg").html('<b style="color:red">Username is already taken.</b>');
+            }
+        }
+    });
+});
---
+++ server/app.js
@@ -0,0 +1,90 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var count = 0;
+var usernames = [];
+var waitinglist = [];
+//new user adding
+function insertval(val){ 
+  var patt = new RegExp("/^\w+$/");
+  var matchtest = patt.test(val);
+  if(matchtest){
+    return 2;
+  }else if(usernames.indexOf(val.toLowerCase()) === -1){
+     usernames.push(val.toLowerCase()); 
+     return 1;
+    }else{ 
+      return -1;
+    }
+}
+
+
+app.get('/', function(req, res){
+  res.send('Test Server');
+});
+
+app.get('/live', function(req, res){
+  res.send(''+(count+1));
+});
+
+app.post('/adduser', function(req, res){
+  if(insertval(req.body.myusername) > 0){
+    res.send('1');
+  }else{
+    res.send('0');
+  }
+});
+
+io.on('connection', function(socket){
+  var username = "";
+  count++;
+  //add user and send and recieve messages
+  socket.on("init",function(data){
+    socket.username = data.username;
+    username = socket.username;
+    console.log(username+" has connected");
+    //algo to allot partner or push to waiting list
+    if(waitinglist.length > 0){
+      partner=waitinglist[0];
+      socket.partner=partner;
+      socket.partner.partner = socket;
+      waitinglist.splice(0,1);
+      socket.broadcast.to(partner.id).emit("partner", {id:socket.id,username:socket.username});
+      io.to(socket.id).emit("partner", {id:partner.id,username:partner.username});
+    }else{
+        waitinglist.push(socket);
+    }
+  });
+
+
+  // currently implemented a broadcast, after algo, this will become individual broadcast emit
+  socket.on("msg",function(msg){
+    // sender(username, id), to (username, id), message
+    socket.broadcast.to(msg.to).emit("msg", msg.username + ' : ' + msg.message);
+  });
+
+  //disconnect
+  socket.on('disconnect', function(){
+    console.log(socket.username +' has disconnected' );
+     if(typeof socket.partner.id === 'undefined') {
+    }else{
+        io.to(socket.partner.id).emit("pat_disc", {});
+    }
+    var index = waitinglist.indexOf(socket);
+    if (index !== -1){
+      waitinglist.splice(index, 1);
+    }
+    index = usernames.indexOf(socket.username);
+    if (index !== -1){
+      usernames.splice(index, 1);
+    }
+    count--;
+  });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
